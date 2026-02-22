04953b410951bd3ca100414b62cfacf200ea5299
unknown <overlordxanime0@gmail.com>
2026-02-22 19:53:51 +0400

    alr almost done w whackamole

--- scripts/dungeon.gd
+++ scripts/dungeon.gd
@@ -12,3 +12,10 @@ func _on_keygame_pressed():
 
 func _on_close_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/main.tscn")
+
+
+func _on_whackamolegame_pressed() -> void:
+	if Global.currency >= 10:
+		get_tree().change_scene_to_file("res://scenes/whackamolegame.tscn")
+		Global.currency -= 10
+		Global.save_game()
---
+++ scripts/wmmanager.gd
@@ -0,0 +1,74 @@
+extends GridContainer
+
+@onready var holes = []
+var active_hole = null
+var score = 0
+var time_left = 60
+var game_running = true
+
+func _ready() -> void:
+	for child in get_children():
+		if child is Button:
+			holes.append(child)
+			child.pressed.connect(_on_button_pressed.bind(child))
+	
+	start_timer()
+	start_game()
+
+func start_timer():
+	while time_left > 0:
+		await get_tree().create_timer(1.0).timeout
+		time_left -= 1
+		$"../timer".text = "Time: %d" % time_left
+	
+	end_game()
+
+func start_game():
+	while game_running:
+		await spawn_mole()
+		await get_tree().create_timer(0.5).timeout
+
+func spawn_mole():
+	if not game_running:
+		return
+	
+	if active_hole:
+		var old_sprite = active_hole.get_node("AnimatedSprite2D")
+		old_sprite.play("default")
+		active_hole = null
+	
+	var hole = holes[randi() % holes.size()]
+	active_hole = hole
+	
+	var sprite = hole.get_node("AnimatedSprite2D")
+	sprite.play("mole")
+	
+	await get_tree().create_timer(1.0).timeout
+	
+	if active_hole == hole and game_running:
+		sprite.play("default")
+		active_hole = null
+
+func _on_button_pressed(hole):
+	if not game_running:
+		return
+	
+	if hole == active_hole:
+		score += 1
+		$"../score".text = "Score: %d/25" % score
+		
+		var sprite = hole.get_node("AnimatedSprite2D")
+		sprite.play("default")
+		active_hole = null
+
+
+func end_game():
+	game_running = false
+	
+	if active_hole:
+		var sprite = active_hole.get_node("AnimatedSprite2D")
+		sprite.play("default")
+		active_hole = null
+	
+	print("GAME OVER")
+	print("Final Score:", score)
